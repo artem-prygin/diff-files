8d66c5e38ad186b66eb341aed97f7ded2cba54a8
Artem Prygin <a.prygin@wellyes.ru>
2020-12-31 14:42:04 +0300

    feedback #5 small fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,6 +3,7 @@ import gendiff from '../index.js';
 
 const generatePathname = (path) => new URL(`../__fixtures__/${path}`, import.meta.url).pathname;
 const testsSuccessOptions = [
+  ['file1.json', 'file2.yml', 'default'],
   ['file1.json', 'file2.yml', 'stylish'],
   ['file1.yml', 'file2.json', 'plain'],
   ['file1.yml', 'file2.json', 'json'],
@@ -16,11 +17,18 @@ const testsFailOptions = [
 describe('gendiff', () => {
   test.each(testsSuccessOptions)('gendiff for %s & %s in %s format',
     (file1, file2, formatName) => {
+      if (formatName === 'default') {
+        const fileContent = fs.readFileSync(generatePathname('expected_stylish.txt'), 'utf-8');
+        expect(gendiff(generatePathname(file1), generatePathname(file2)))
+          .toBe(fileContent);
+        return;
+      }
       if (formatName === 'json') {
         expect(() => {
           JSON.parse(gendiff(generatePathname(file1), generatePathname(file2), formatName));
         }).not.toThrowError();
       }
+
       const fileContent = fs.readFileSync(generatePathname(`expected_${formatName}.txt`), 'utf-8');
       expect(gendiff(generatePathname(file1), generatePathname(file2), formatName))
         .toBe(fileContent);
